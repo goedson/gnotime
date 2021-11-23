c20f3364af3af4c16b53c983da1beffedf930725
markuspg <markuspg@users.noreply.github.com>
2021-11-23 22:31:09 +0100

    Drop GnomeVFS from ghtml.c

--- src/ghtml.c
+++ src/ghtml.c
@@ -26,7 +26,6 @@
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
-#include <libgnomevfs/gnome-vfs.h>
 
 #include <qof.h>
 
@@ -43,6 +42,7 @@
 #include "query.h"
 #include "util.h"
 
+#include <gio/gio.h>
 
 /* Design problems:
  * The way this is currently defined, there is no type safety, and
@@ -1547,32 +1547,51 @@ gtt_ghtml_display (GttGhtml *ghtml, const char *filepath,
 	}
 
 	/* Try to get the ghtml file ... */
-	GnomeVFSResult    result;
-	GnomeVFSHandle   *handle;
-	result = gnome_vfs_open (&handle, filepath, GNOME_VFS_OPEN_READ);
-	if ((GNOME_VFS_OK != result) && (0==ghtml->open_count))
+	GError *error = NULL;
+	GFile *ifile = g_file_new_for_path (filepath);
+	GFileInputStream *istream = g_file_read (ifile, NULL, &error);
+	if ((NULL == istream) && (0==ghtml->open_count))
 	{
 		if (ghtml->error)
 		{
 			(ghtml->error) (ghtml, 404, filepath, ghtml->user_data);
 		}
+		g_object_unref (ifile);
+		ifile = NULL;
 		return;
 	}
 	ghtml->ref_path = filepath;
 
 	/* Read in the whole file.  Hopefully its not huge */
 	template = g_string_new (NULL);
-	while (GNOME_VFS_OK == result)
+	while (TRUE)
 	{
 #define BUFF_SIZE 4000
 		char buff[BUFF_SIZE+1];
-		GnomeVFSFileSize  bytes_read;
-		result = gnome_vfs_read (handle, buff, BUFF_SIZE, &bytes_read);
-		if (0 >= bytes_read) break;  /* EOF I presume */
-		buff[bytes_read] = 0x0;
+		const gssize read_bytes = g_input_stream_read (G_INPUT_STREAM(istream), buff,
+																									 BUFF_SIZE, NULL, &error);
+		if (0 > read_bytes)
+		{
+			g_warning("Failed to read HTML file \"%s\": %s", filepath, error->message);
+			g_error_free (error);
+			error = NULL;
+			break;
+		}
+		if (0 == read_bytes)
+		{
+			break; // EOF
+		}
+		buff[read_bytes] = 0x0;
 		g_string_append (template, buff);
 	}
-	gnome_vfs_close (handle);
+	if (!g_input_stream_close (G_INPUT_STREAM (istream), NULL, &error))
+	{
+		g_warning ("Failed to close HTML file \"%s\": %s", filepath, error->message);
+	}
+	g_object_unref (istream);
+	istream = NULL;
+	g_object_unref (ifile);
+	ifile = NULL;
 
 	/* ugh. gag. choke. puke. */
 	ghtml_guile_global_hack = ghtml;
